8fb4c505cca9bd7ba9d4faacc46fd05ad01f486a
kacperszyca <kacperszyca11@gmail.com>
2020-10-16 14:24:44 +0200

    first commit

---
+++ NAI1/main.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640) #id 3 = szerokosc
+cap.set(4,480) #id 4 = wysokosc
+
+while True:
+    success,  img = cap.read()  # czytanie kratek
+    imgMirroring = cv2.flip(img,1) # odbicie lustrzane
+    cv2.imshow("Normalne",img)
+    cv2.imshow("Odbite", imgMirroring)
+    key = cv2.waitKey(33) # https://stackoverflow.com/questions/14494101/using-other-keys-for-the-waitkey-function-of-opencv
+    if key==27:
+        break
+    elif key==-1: # normalnie zwraca -1
+        continue
+
+
+
+
