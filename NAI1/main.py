5c0d55ba572763f3e209ee1a3614b733204aa08c
kacperszyca <kacperszyca11@gmail.com>
2020-10-20 12:56:12 +0200

    Dodano run.sh + testowe video

--- NAI1/main.py
+++ NAI1/main.py
@@ -2,12 +2,17 @@ import cv2
 
 
 #cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("Resources/video.mp4")
-cap.set(3,640) #id 3 = szerokosc
-cap.set(4,480) #id 4 = wysokosc
-
-while True:
+cap = cv2.VideoCapture("resources/video.mp4")
+cap.set(3,300) #id 3 = szerokosc
+cap.set(4,300) #id 4 = wysokosc
+frame_counter = 0
+while (cap.isOpened()):
     success,  img = cap.read()  # czytanie kratek
+    frame_counter += 1
+    #If the last frame is reached, reset the capture and the frame_counter
+    if frame_counter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        frame_counter = 0 #Or whatever as long as it is the same as next line
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
     imgMirroring = cv2.flip(img,1) # odbicie lustrzane
     cv2.imshow("Normalne",img)
     cv2.imshow("Odbite", imgMirroring)
---
+++ NAI1/run.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3
+sudo apt-get install python-pip
+sudo apt-get install python3-opencv
+python3 main.py
